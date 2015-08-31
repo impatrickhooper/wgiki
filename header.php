f0e9ff0b06828f26ce0678f5e2364fb1d887c780
Patrick Hooper <patrick.d.hooper@gmail.com>
2015-08-31 09:51:50 -0400

    Removed Google Tag Manager

--- header.php
+++ header.php
@@ -22,24 +22,14 @@
 
 <body <?php body_class(); ?>>
 
-<!-- Google Tag Manager -->
-<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-TNRGSD"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TNRGSD');</script>
-<!-- End Google Tag Manager -->
-
 <?php $page_division = getPageDivision(); ?>
 
   <div id="page" class="<?php echo $page_division; ?> hfeed site">
     <header id="masthead" class="site-header clearfix" role="banner">
 
     <?php
-        /* Output the navbar */
-        get_template_part('template-parts/content_wgiki', 'navbar');
+      /* Output the navbar */
+      get_template_part('template-parts/content_wgiki', 'navbar');
 
       /* Output side nav if user is logged in */
       if (is_user_logged_in()) {
